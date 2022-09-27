f08438b0e3cb0aebf1b6416e79fd017b1bd0832d
FKay <fatima_kharodia12@hotmail.com>
2022-09-27 09:08:14 -0700

    Added player functions and game function

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function getComputerChoice(arr) {
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+//to read computer choice as a string
+let comp="";
+
+//Player Input 
+let playerSelection = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
+if (playerSelection == 'Rock' || playerSelection == 'Paper' || playerSelection == 'Scissors') {
+    console.log(`You Chose: ${playerSelection}`);
+} else {
+    console.log("Error, Try Again");
+}
+
+
+
+//Random Computer Choice
+function computerSelection(arr) {
 
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
-    
-    return item;
+    comp = arr[randomIndex];
+
+    return item.toString();
 }
 
 const array = ['Rock', 'Paper', 'Scissors'];
-const result = getComputerChoice(array);
-console.log(result);
+const result = computerSelection(array);
+console.log('Computer Chose: ' + result);
+
+
+
+function playRound() {
+    
+    if (
+    (playerSelection == 'Rock' && comp == 'Scissors') ||
+    (playerSelection == 'Paper' && comp == 'Rock') ||
+    (playerSelection == 'Scissors' && comp == 'Paper')
+    ) {
+        console.log("You Win");
+    }
+    else if  (playerSelection == comp) {
+        console.log("Tie Game!");
+    }
+    else {
+        console.log("Computer Wins!");
+    }
+    console.log("Refresh Page to Play Again!");
+}
+
+playRound();
